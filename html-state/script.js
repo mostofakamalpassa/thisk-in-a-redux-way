eee3b88890410ebd23dd66bcc23bbf2450181719
mostofa kamal <mostofakamal1990@gmail.com>
2022-08-13 01:05:07 +0600

    counter  state useing by vanalla js

---
+++ html-state/script.js
@@ -0,0 +1,22 @@
+const counterEl = document.getElementById('counter');
+
+const incrementEl = document.getElementById('increment');
+const decrementEl = document.getElementById('decrement');
+
+
+// initial state 
+
+let count = 0;
+
+// event listeners
+
+incrementEl.addEventListener('click', ()=>{
+    count++;
+    counterEl.innerText = count;
+
+});
+
+decrementEl.addEventListener('click', ()=>{
+    count--;
+    counterEl.innerText = count;
+})
