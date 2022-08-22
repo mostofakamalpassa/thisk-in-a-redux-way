205a7ab7ab000acad5da2ca66d8cfd7e27ec57e7
mostofa kamal <mostofakamal1990@gmail.com>
2022-08-22 06:51:16 +0600

    todod reducer and filter reducer add

---
+++ todoapp/src/redux/filters/reducer.js
@@ -0,0 +1,43 @@
+import {STATUS_CHANGED, COLOR_CHANGED} from './actionTypes';
+
+const initalStatie = {
+    status:'All',
+    color:['red','green']
+}
+
+
+const reducer = (state = initalStatie, action) =>{
+        switch( action.type){
+
+            case STATUS_CHANGED :
+                return{
+                    ...state,
+                    status: action.payload.status
+                }
+            
+                case COLOR_CHANGED:
+                    const {color, changeType} = action.payload;
+                    switch(changeType){
+                        case 'added':
+                            return{
+                                ...state,
+                                colors:[
+                                    ...state.colors,
+                                    color
+                                ]
+                            }
+                        case 'removed':
+                            return{
+                                ...state,
+                                colors: state.colors.filter(existingColor => existingColor !== color)
+                            }
+                        
+                        default:
+                            return state;
+                    }
+            default:
+                return state;
+        }
+}
+
+export default reducer;
